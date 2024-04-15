815ca3993302dc15bc6e5553584b54254680fe70
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-04-16 02:36:43 +0700

    Add conan for dependency management and update CMakeLists.txt

---
+++ conanfile.py
@@ -0,0 +1,12 @@
+from conan import ConanFile
+
+class Recipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+
+    def layout(self):
+        self.folders.generators = "conan"
+
+    def requirements(self):
+        self.requires("glfw/3.4")
+        self.requires("glad/0.1.36")
